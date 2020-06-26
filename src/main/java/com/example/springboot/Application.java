6483a4baff8c9d4e6f3a26817f9144f3b5d316ff
unknown <cbq3737@naver.com>
2020-06-26 10:48:10 +0900

    as

--- src/main/java/com/example/springboot/Application.java
+++ src/main/java/com/example/springboot/Application.java
@@ -2,10 +2,9 @@ package com.example.springboot;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
 //@EnableJpaAuditing
-@SpringBootApplication(exclude = DataSourceAutoConfiguration.class)
+@SpringBootApplication
 public class Application {
     public static void main(String[] args)
     {
